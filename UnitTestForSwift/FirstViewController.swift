d7bf29e57deae58607395e7018454f83be016f6e
Lei Huang <leisure.huang34@gmail.com>
2017-09-04 10:09:49 +0800

    Initial Commit

---
+++ UnitTestForSwift/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  UnitTestForSwift
+//
+//  Created by Lei Huang on 04/09/2017.
+//  Copyright © 2017 thoughtworks. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
